adea53aee520999a3ae07237751162c2fd6d0be8
Elpidio Julian <elpidiojulian@gmail.com>
2023-06-08 10:19:39 -0500

    added loginUser and registerUser POST requests

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,44 @@
 const baseURL = 'local:host3000/api'; // to be changed later
 
+export const registerUser = async (username, password) => {
+    try {
+      const response = await fetch(`${baseURL}/users/register`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+  
+          username: username,
+          password: password
+  
+        })
+      })
+      const result = await response.json();
+  
+      return result;
+    } catch (error) {
+      console.log('error registering user')
+    }
+  }
+
+  export const loginUser = async (username, password) => {
+    try {
+      const response = await fetch(`${baseURL}/users/login`, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+  
+          username: username,
+          password: password
+  
+        })
+      })
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.log('error logging in user')
+    }
+  }
