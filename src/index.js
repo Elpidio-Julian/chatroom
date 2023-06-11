1e331aad5b34998f65a695f91a53f07527c43c61
Elpidio Julian <elpidiojulian@gmail.com>
2023-06-10 19:13:58 -0500

    migrated chatpage code to components

--- src/index.js
+++ src/index.js
@@ -6,27 +6,11 @@ import './style.css';
 
 
 
-const App = () => {
+const ChatWindow = ({ messages, isConnected }) => {
 
-  // message state for holding messages from server
-  const [messages, setMessages] = useState([
-    {
-      message: 'Welcome to the Chatter',
-      edited: false,
-      author: 'System'
-    }
-  ]);
 
   // message state for dynamically updating message to be sent in js
   const [message, setMessage] = useState('');
-
-  // functions for connecting/disconnecting from socket
-  function connect() {
-    socket.connect();
-  }
-  function disconnect() {
-    socket.disconnect();
-  }
 
   // sending message function
   function sendMessage(message) {
@@ -35,54 +19,8 @@ const App = () => {
     setMessage('');
   }
 
-  // socket io chat message event receiver
-  // socket.on('chat message', function(msg) {
-  //   // currently updates state far too many times
-  //   setMessages(messages => [...messages, {message: msg}])
-  //   console.log(messages)
-  // })
-
-  // state for dynamically letting user know if they are connected and for button to keep track of connection
-  // socket should not connect automatically so the state should start as false
-  const [isConnected, setIsConnected] = useState(false)
-
-  // This useEffect has event listeners and changes the state
-  // Use this to send socket events without running them multiple times as state updates
-  useEffect(() => {
-    // socket helper functions
-    function onConnect() {
-      setIsConnected(true);
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    function onChatMessage(msg) {
-      setMessages(messages => [...messages, {message: msg}])
-    }
-   
-    // socket event handler
-    socket.on('chat message', function(msg) {
-      onChatMessage(msg)
-    })
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-    };
-  }, []);
-  console.log(messages)
-
   return (
-    <React.Fragment>
-      <header>Hello World</header>
-      {!isConnected ? <button type="button" className="btn btn-success" onClick={() => connect()}>Connect</button>
-        : <button type="button" className="btn btn-danger" onClick={() => disconnect()}>Disconnect</button>
-      }
-      <div className="container pt-3 align-items-center vh-100">
+    <div className="container pt-3 align-items-center vh-100">
         <div className="row align-items-start justify-content-center overflow-auto h-75 border border-primary m-1">
           <ul id="chatWindow" className="list-group list-group-flush">
             {messages.map((msg, idx) => {
@@ -132,6 +70,103 @@ const App = () => {
             </div>
           </form>
       </div>
+  )
+}
+
+
+
+
+const ChatPage = ({isConnected, setIsConnected}) => {
+
+  const [messages, setMessages] = useState([
+    {
+      message: 'Welcome to the Chatter',
+      edited: false,
+      author: 'System'
+    }
+  ]);
+  
+  // functions for connecting/disconnecting from socket
+  function connect() {
+    socket.connect();
+  }
+  function disconnect() {
+    socket.disconnect();
+  }
+
+  // socket io chat message event receiver
+  // socket.on('chat message', function(msg) {
+  //   // currently updates state far too many times
+  //   setMessages(messages => [...messages, {message: msg}])
+  //   console.log(messages)
+  // })
+
+  useEffect(() => {
+    // socket helper functions
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onChatMessage(msg) {
+      setMessages(messages => [...messages, {message: msg}])
+    }
+   
+    // socket event handler
+    socket.on('chat message', function(msg) {
+      onChatMessage(msg)
+    })
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      {!isConnected ? <button type="button" className="btn btn-success" onClick={() => connect()}>Connect</button>
+        : <button type="button" className="btn btn-danger" onClick={() => disconnect()}>Disconnect</button>
+      }
+
+      <ChatWindow messages={messages} isConnected={isConnected}/>
+
+    </React.Fragment>
+    
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const App = () => {
+
+  // state for dynamically letting user know if they are connected and for button to keep track of connection
+  // socket should not connect automatically so the state should start as false
+  const [isConnected, setIsConnected] = useState(false)
+
+
+  return (
+    <React.Fragment>
+      <header>Hello World</header>
+      <ChatPage isConnected={isConnected} setIsConnected={setIsConnected}/>
     </React.Fragment>
   )
 }
